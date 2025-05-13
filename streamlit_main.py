6a371e70df994b04eec6b3473d7ebe00206020ce
petersvorc <peter.svorc@apsp.sk>
2025-05-13 12:04:15 +0200

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -31,17 +31,22 @@ df_cennik = nacitaj_data(CENNIK_SHEET)
 df_sluzby = nacitaj_data(SLUZBY_SHEET)
 
 # Funkcia na výpočet ceny dlažby
-def vypocitaj_cenu_dlazby(rozmer, hrubka, povrch, mnozstvo):
-    filtr = df_cennik[(df_cennik["rozmery (mm)"] == rozmer) & (df_cennik["hrúbka (mm)"] == hrubka) & (df_cennik["povrch"] == povrch)]
+def vypocitaj_cenu_dlazby(param, mnozstvo):
+    filtr = df_cennik[df_cennik["rozmer + hrúbka + povrch"] == param]
     if mnozstvo <= 20:
-        cena = filtr.iloc[0]["21-59 m2"] + filtr.iloc[0]["doprava (paleta)"]
+        cena_za_m2 = filtr.iloc[0]["21-59 m2"]
+        doprava = filtr.iloc[0]["transportná paleta"] + filtr.iloc[0]["doprava"]
+        celkova_cena = round(cena_za_m2 * mnozstvo + doprava)
     elif 21 <= mnozstvo <= 59:
-        cena = filtr.iloc[0]["21-59 m2"]
+        cena_za_m2 = filtr.iloc[0]["21-59 m2"]
+        celkova_cena = round(cena_za_m2 * mnozstvo)
     elif 60 <= mnozstvo <= 120:
-        cena = filtr.iloc[0]["60-120 m2"]
-    else:
-        cena = filtr.iloc[0]["60-120 m2"]  # + individuálna zľava (rieši sa individuálne)
-    return round(cena * mnozstvo)
+        cena_za_m2 = filtr.iloc[0]["60-120 m2"]
+        celkova_cena = round(cena_za_m2 * mnozstvo)
+    else:  # nad 121 m2
+        cena_za_m2 = filtr.iloc[0]["60-120 m2"]
+        celkova_cena = round(cena_za_m2 * mnozstvo)
+    return celkova_cena, mnozstvo
 
 # Funkcia na výpočet ceny služieb
 def vypocitaj_cenu_sluzieb(sluzby):
@@ -57,7 +62,7 @@ def main():
     # Výber dekoru
     dekor = st.selectbox("Vyberte dekor:", sorted(df_formaty["dekor"].unique()))
     df_kolekcie = df_formaty[df_formaty["dekor"] == dekor]
-    
+
     # Výber kolekcie
     kolekcia = st.selectbox("Vyberte kolekciu:", sorted(df_kolekcie["kolekcia"].unique()))
     df_serie = df_kolekcie[df_kolekcie["kolekcia"] == kolekcia]
@@ -66,12 +71,8 @@ def main():
     seria = st.selectbox("Vyberte sériu:", sorted(df_serie["séria"].unique()))
     df_rozmery = df_serie[df_serie["séria"] == seria]
 
-    # Výber rozmeru
-    rozmer = st.selectbox("Vyberte rozmer:", sorted(df_rozmery["rozmery (mm)"].unique()))
-    df_hrubky = df_rozmery[df_rozmery["rozmery (mm)"] == rozmer]
-
-    # Výber povrchu
-    povrch = st.selectbox("Vyberte povrch:", sorted(df_hrubky["povrch"].unique()))
+    # Výber rozmeru + hrúbka + povrch
+    param = st.selectbox("Vyberte formát + povrch:", sorted(df_rozmery["rozmer + hrúbka + povrch"].unique()))
 
     # Zadanie množstva
     mnozstvo = st.number_input("Zadajte množstvo v m²:", min_value=1, step=1)
@@ -84,21 +85,25 @@ def main():
     miesto = st.text_input("Zadajte miesto dodania:")
 
     if st.button("Odoslať dopyt"):
-        cena_dlazby = vypocitaj_cenu_dlazby(rozmer, df_hrubky.iloc[0]["hrúbka (mm)"], povrch, mnozstvo)
+        cena_dlazby, mnozstvo_zaznam = vypocitaj_cenu_dlazby(param, mnozstvo)
         cena_sluzieb = vypocitaj_cenu_sluzieb(vybrane_sluzby)
         celkova_cena = cena_dlazby + cena_sluzieb
 
         # Súhrn položiek
-        suhrn = f"Dekor: {dekor}, Kolekcia: {kolekcia}, Séria: {seria}, Rozmer: {rozmer}, Povrch: {povrch}, Množstvo: {mnozstvo} m²"
+        suhrn = f"Dekor: {dekor}, Kolekcia: {kolekcia}, Séria: {seria}, Rozmer: {param}, Množstvo: {mnozstvo} m²"
         if vybrane_sluzby:
             suhrn += ", Služby: " + ", ".join(vybrane_sluzby)
+
+        # Poznámka pri individuálnej zľave
+        if mnozstvo > 121:
+            suhrn += " (Cena bude individuálne upravená)"
 
         # Zápis do Google Sheets
         sheet = client.open(SHEET_NAME).worksheet(DOPYT_SHEET)
         datum = datetime.datetime.now().strftime("%Y-%m-%d")
         id_zaujemcu = f"zaujemca_{int(datetime.datetime.now().timestamp())}"
 
-        novy_zaznam = [datum, id_zaujemcu, email, miesto, dekor, df_hrubky.iloc[0]["značka"], kolekcia, seria, rozmer, df_hrubky.iloc[0]["hrúbka (mm)"], povrch, mnozstvo, celkova_cena, suhrn]
+        novy_zaznam = [datum, id_zaujemcu, email, miesto, dekor, df_rozmery.iloc[0]["značka"], kolekcia, seria, param, mnozstvo, celkova_cena, suhrn]
         sheet.append_row(novy_zaznam)
 
         st.success("Dopyt bol úspešne odoslaný! Ďakujeme.")
