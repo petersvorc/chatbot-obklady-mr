db817fdb646425e3781cea55258a3006120754c5
petersvorc <peter.svorc@apsp.sk>
2025-05-19 23:42:48 +0200

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -32,30 +32,31 @@ def generate_id():
 def vypocitaj_cenu(param, mnozstvo, celkove_mnozstvo):
     riadok = df_cennik[df_cennik["rozmer + hrúbka + povrch"] == param]
     if riadok.empty:
-        return None, None, None, None
+        return None, None, None, None, None
     doprava = 0
     doprava_text = None
+    doprava_cena = 0
     if celkove_mnozstvo <= 20:
-        cena_m2 = riadok.iloc[0]["21-59 m2"]
+        cena_m2 = round(riadok.iloc[0]["21-59 m2"])
         doprava_riadok = df_doprava[df_doprava["polozka"].str.lower() == "doprava do 20 m²"]
         if not doprava_riadok.empty:
-            doprava = round(float(doprava_riadok["cena"].values[0]))
-            doprava_text = f"{len(st.session_state['vybrane_dlazby']) + 1}. doprava do 20 m² | {doprava} €"
-        cena = round(cena_m2 * mnozstvo + doprava)
+            doprava_cena = round(float(doprava_riadok["cena"].values[0]))
+            doprava_text = f"doprava do 20 m²"
+        cena = round(cena_m2 * mnozstvo)
         poznamka = None
     elif 21 <= celkove_mnozstvo <= 59:
-        cena_m2 = riadok.iloc[0]["21-59 m2"]
+        cena_m2 = round(riadok.iloc[0]["21-59 m2"])
         cena = round(cena_m2 * mnozstvo)
         poznamka = None
     elif 60 <= celkove_mnozstvo <= 120:
-        cena_m2 = riadok.iloc[0]["60-120 m2"]
+        cena_m2 = round(riadok.iloc[0]["60-120 m2"])
         cena = round(cena_m2 * mnozstvo)
         poznamka = None
     else:
-        cena_m2 = riadok.iloc[0]["60-120 m2"]
+        cena_m2 = round(riadok.iloc[0]["60-120 m2"])
         cena = round(cena_m2 * mnozstvo)
         poznamka = "Pri množstve dlažby nad 121 m2 je pravdepodobne priestor pre zľavu. Odošlite formulár alebo nás priamo kontaktujte."
-    return round(cena_m2), cena, poznamka, doprava_text
+    return cena_m2, cena, poznamka, doprava_text, doprava_cena
 
 # ------------------- Formulár -------------------
 st.header("Overte si parametre našich dlažieb a orientačné ceny.")
@@ -67,7 +68,7 @@ vybrane_sluzby = st.multiselect("Vyberte doplnkové služby:", df_sluzby["sluzba
 
 if st.button("Pridať tento typ dlažby"):
     celkove_mnozstvo = sum(p["mnozstvo"] for p in st.session_state["vybrane_dlazby"]) + mnozstvo
-    cena_m2, cena, poznamka, doprava_text = vypocitaj_cenu(param, mnozstvo, celkove_mnozstvo)
+    cena_m2, cena, poznamka, doprava_text, doprava_cena = vypocitaj_cenu(param, mnozstvo, celkove_mnozstvo)
     if cena is None:
         st.error("Nenašla sa cena pre vybraný formát.")
     else:
@@ -78,6 +79,7 @@ if st.button("Pridať tento typ dlažby"):
             "cena": cena,
             "poznamka": poznamka,
             "doprava_text": doprava_text,
+            "doprava_cena": doprava_cena,
             "sluzby": vybrane_sluzby,
             "cena_sluzby": sum(round(df_sluzby[df_sluzby["sluzba"] == s]["cena"].values[0]) for s in vybrane_sluzby)
         }
@@ -92,8 +94,9 @@ if st.session_state["vybrane_dlazby"]:
     for p in st.session_state["vybrane_dlazby"]:
         st.write(f"{counter}. {p['param']} | {p['cena_m2']} €/m² | {p['mnozstvo']} m² | {p['cena']} €")
         total += p["cena"]
-        if p["doprava_text"]:
-            st.write(p["doprava_text"])
+        if p["doprava_text"] and p["doprava_cena"]:
+            st.write(f"{counter}. {p['doprava_text']} | {p['doprava_cena']} €")
+            total += p["doprava_cena"]
         for sluzba in p['sluzby']:
             cena_sluzby = round(df_sluzby[df_sluzby["sluzba"] == sluzba]["cena"].values[0])
             st.write(f"{counter}. Doplnková služba: {sluzba} | {cena_sluzby} €")
@@ -104,7 +107,7 @@ if st.session_state["vybrane_dlazby"]:
     st.write(f"**Orientačná cena spolu:** {total} €")
 
     if st.button("Vybrať ďalšiu dlažbu"):
-        st.experimental_rerun()
+        st.rerun()
 
     st.subheader("Zadajte kontaktné údaje:")
     email = st.text_input("Emailová adresa:")
@@ -128,7 +131,7 @@ if st.session_state["vybrane_dlazby"]:
                     "",
                     p["param"],
                     p["mnozstvo"],
-                    p["cena"],
+                    p["cena"] + p["doprava_cena"] + p["cena_sluzby"],
                     f"{p['param']} | {p['cena_m2']} €/m² | {p['mnozstvo']} m² | Služby: {', '.join(p['sluzby']) if p['sluzby'] else 'Žiadne'}"
                 ])
             st.success("Dopyt bol odoslaný.")
